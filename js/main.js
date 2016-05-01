a9a5262f325700c7cd23e4eef72a5bbe8905630c
Pehesi97 <pedro.henrique.silva.1997@outlook.com.br>
2016-05-01 04:30:41 -0300

    Adds the randomizer core

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function() {
-    var app = angular.module('tournamizr', []);
+    var app = angular.module('tournamizer', []);
 
     app.controller('TabsController', function() {
         this.setTab = 1;
@@ -9,30 +9,91 @@
         };
     });
 
-    app.controller('TimesTabController', [ '$http', function($http) {
+    app.controller('TimesTabController', [ '$http', '$timeout', function($http, $timeout) {
         var that = this;
 
-        this.quantity = 1;
+        this.quantity = 3;
         this.players = 5;
 
-        this.randomizing = false;
+        this.randomizing = 0;
+        this.progress = 0;
         this.createProgress = '0';
+        this.errorCreate = false;
 
         this.jogadores = [ ];
+        this.step = 0;
 
         $http.get('data/players.json').success(function(data) {
             that.jogadores = data;
+            that.step = that.jogadores.length / (that.quantity * that.players);
         });
 
-        this.create = function(tabs) {
-            if(that.jogadores.length == that.quantity * that.players) {
-                that.randomizing = true;
-                that.createProgress = '20%';
-                //tabs.setTab = 2;
+        this.create = function(tabs, times) {
+            if(that.jogadores.length < (that.quantity * that.players)) {
+                that.errorCreate = true;
+                return;
             }
-            else {
 
+            that.randomizing = 1;
+            this.playerArray = [];
+            this.possibleIndexes = [];
+            var j;
+            for(j = that.quantity; j < that.jogadores.length; j++)
+            {
+                this.possibleIndexes.push(j);
             }
+            var i = 0, r = 0, c = 0;
+            for(i = 0; i < that.quantity; i++)
+            {
+                this.playerArray = [ ];
+                this.playerArray.push(that.jogadores[i].name);
+
+                for(c = 1; c < that.players; c++)
+                {
+                    r = Math.floor((Math.random() * (this.possibleIndexes.length)));
+                    this.playerArray.push(that.jogadores[this.possibleIndexes[r]].name);
+                    this.possibleIndexes.splice(r, 1);
+                }
+                times.addTime(this.playerArray);
+            }
+            that.randomizing = 2;
+            tabs.setTab = 2;
         }
     } ]);
+
+    app.controller('TimesController', function() {
+        var that = this;
+
+        function Time() {
+            this.id;
+            this.jogadores = [];
+
+            this.setId = function(id) {
+                this.id = id;
+            }
+
+            this.add = function(name) {
+                this.jogadores.push(name);
+            }
+
+            this.print = function() {
+                console.log(this.jogadores);
+            }
+        }
+
+        this.times = [ ];
+        this.lastId = 1;
+
+        this.addTime = function(jogadores) {
+            var time = new Time();
+            time.setId(that.lastId);
+            var i;
+            for(i = 0; i < jogadores.length; i++)
+                time.add(jogadores[i]);
+
+            that.times.push(time);
+            that.lastId++;
+        }
+
+    });
 })();
